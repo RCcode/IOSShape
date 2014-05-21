f2af57604addfb1233e0ffbfb3f333da0d952aee
wsq-wlq <wsq-wlq@wsq-wlqdeMacBook-Pro.local>
2014-05-21 19:52:37 +0800

    Initial Commit

---
+++ IOSShape/PHO_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PHO_AppDelegate.h
+//  IOSShape
+//
+//  Created by wsq-wlq on 14-5-21.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHO_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOSShape/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOSShape
+//
+//  Created by wsq-wlq on 14-5-21.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PHO_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PHO_AppDelegate class]));
+    }
+}
