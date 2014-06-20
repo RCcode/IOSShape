8d5fe3cb345b464a186b3c7644e25d5c58cba051
wsq-wlq <wsq-wlq@wsq-wlqdeMacBook-Pro.local>
2014-06-20 20:31:24 +0800

    图片反转

--- IOSShape/PHO_HomeViewController.m
+++ IOSShape/PHO_HomeViewController.m
@@ -224,84 +224,81 @@
         chooseImage = [info objectForKey:UIImagePickerControllerOriginalImage];
         
     }
-    else
+    /*改变图片旋转90度问题*/
+    UIImage *cameraPic = [info objectForKey:UIImagePickerControllerOriginalImage];
+    //    NSLog(@"cameraPic.imageOrientation == %ld",cameraPic.imageOrientation);
+    
+    if (cameraPic.imageOrientation != UIImageOrientationUp)
     {
-        /*改变图片旋转90度问题*/
-        UIImage *cameraPic = [info objectForKey:UIImagePickerControllerOriginalImage];
-        //    NSLog(@"cameraPic.imageOrientation == %ld",cameraPic.imageOrientation);
         
-        if (cameraPic.imageOrientation != UIImageOrientationUp)
+        int mi = cameraPic.imageOrientation;
+        // We need to calculate the proper transformation to make the image upright.
+        // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
+        CGAffineTransform transform = CGAffineTransformIdentity;
+        
+        switch (mi)
         {
-            
-            int mi = cameraPic.imageOrientation;
-            // We need to calculate the proper transformation to make the image upright.
-            // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
-            CGAffineTransform transform = CGAffineTransformIdentity;
-            
-            switch (mi)
-            {
-                case UIImageOrientationDown:
-                case UIImageOrientationDownMirrored:
-                    transform = CGAffineTransformTranslate(transform, cameraPic.size.width, cameraPic.size.height);
-                    transform = CGAffineTransformRotate(transform, M_PI);
-                    break;
-                    
-                case UIImageOrientationLeft:
-                case UIImageOrientationLeftMirrored:
-                    transform = CGAffineTransformTranslate(transform, cameraPic.size.width, 0);
-                    transform = CGAffineTransformRotate(transform, M_PI_2);
-                    break;
-                    
-                case UIImageOrientationRight:
-                case UIImageOrientationRightMirrored:
-                    transform = CGAffineTransformTranslate(transform, 0, cameraPic.size.height);
-                    transform = CGAffineTransformRotate(transform, -M_PI_2);
-                    break;
-            }
-            
-            switch (mi)
-            {
-                case UIImageOrientationUpMirrored:
-                case UIImageOrientationDownMirrored:
-                    transform = CGAffineTransformTranslate(transform, cameraPic.size.width, 0);
-                    transform = CGAffineTransformScale(transform, -1, 1);
-                    break;
-                    
-                case UIImageOrientationLeftMirrored:
-                case UIImageOrientationRightMirrored:
-                    transform = CGAffineTransformTranslate(transform, cameraPic.size.height, 0);
-                    transform = CGAffineTransformScale(transform, -1, 1);
-                    break;
-            }
-            
-            // Now we draw the underlying CGImage into a new context, applying the transform
-            // calculated above.
-            CGContextRef ctx = CGBitmapContextCreate(NULL, cameraPic.size.width, cameraPic.size.height,
-                                                     CGImageGetBitsPerComponent(cameraPic.CGImage), 0,
-                                                     CGImageGetColorSpace(cameraPic.CGImage),
-                                                     CGImageGetBitmapInfo(cameraPic.CGImage));
-            CGContextConcatCTM(ctx, transform);
-            switch (mi)
-            {
-                case UIImageOrientationLeft:
-                case UIImageOrientationLeftMirrored:
-                case UIImageOrientationRight:
-                case UIImageOrientationRightMirrored:
-                    // Grr...
-                    CGContextDrawImage(ctx, CGRectMake(0,0,cameraPic.size.height,cameraPic.size.width), cameraPic.CGImage);
-                    break;
-                    
-                default:
-                    CGContextDrawImage(ctx, CGRectMake(0,0,cameraPic.size.width,cameraPic.size.height), cameraPic.CGImage);
-                    break;
-            }
-            
-            // And now we just create a new UIImage from the drawing context
-            CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
-            chooseImage = [UIImage imageWithCGImage:cgimg];
-            CGContextRelease(ctx);
-            CGImageRelease(cgimg);
+            case UIImageOrientationDown:
+            case UIImageOrientationDownMirrored:
+                transform = CGAffineTransformTranslate(transform, cameraPic.size.width, cameraPic.size.height);
+                transform = CGAffineTransformRotate(transform, M_PI);
+                break;
+                
+            case UIImageOrientationLeft:
+            case UIImageOrientationLeftMirrored:
+                transform = CGAffineTransformTranslate(transform, cameraPic.size.width, 0);
+                transform = CGAffineTransformRotate(transform, M_PI_2);
+                break;
+                
+            case UIImageOrientationRight:
+            case UIImageOrientationRightMirrored:
+                transform = CGAffineTransformTranslate(transform, 0, cameraPic.size.height);
+                transform = CGAffineTransformRotate(transform, -M_PI_2);
+                break;
         }
+        
+        switch (mi)
+        {
+            case UIImageOrientationUpMirrored:
+            case UIImageOrientationDownMirrored:
+                transform = CGAffineTransformTranslate(transform, cameraPic.size.width, 0);
+                transform = CGAffineTransformScale(transform, -1, 1);
+                break;
+                
+            case UIImageOrientationLeftMirrored:
+            case UIImageOrientationRightMirrored:
+                transform = CGAffineTransformTranslate(transform, cameraPic.size.height, 0);
+                transform = CGAffineTransformScale(transform, -1, 1);
+                break;
+        }
+        
+        // Now we draw the underlying CGImage into a new context, applying the transform
+        // calculated above.
+        CGContextRef ctx = CGBitmapContextCreate(NULL, cameraPic.size.width, cameraPic.size.height,
+                                                 CGImageGetBitsPerComponent(cameraPic.CGImage), 0,
+                                                 CGImageGetColorSpace(cameraPic.CGImage),
+                                                 CGImageGetBitmapInfo(cameraPic.CGImage));
+        CGContextConcatCTM(ctx, transform);
+        switch (mi)
+        {
+            case UIImageOrientationLeft:
+            case UIImageOrientationLeftMirrored:
+            case UIImageOrientationRight:
+            case UIImageOrientationRightMirrored:
+                // Grr...
+                CGContextDrawImage(ctx, CGRectMake(0,0,cameraPic.size.height,cameraPic.size.width), cameraPic.CGImage);
+                break;
+                
+            default:
+                CGContextDrawImage(ctx, CGRectMake(0,0,cameraPic.size.width,cameraPic.size.height), cameraPic.CGImage);
+                break;
+        }
+        
+        // And now we just create a new UIImage from the drawing context
+        CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+        chooseImage = [UIImage imageWithCGImage:cgimg];
+        CGContextRelease(ctx);
+        CGImageRelease(cgimg);
     }
 
     if (chooseImage)
--- IOSShape/PHO_MainShowView.m
+++ IOSShape/PHO_MainShowView.m
@@ -184,26 +184,26 @@
     
     if (picture.size.width >= picture.size.height)
     {
-//        if (picture.size.height < 1080 )
-//        {
-//            picture = [picture rescaleImageToSize:CGSizeMake(picture.size.height*scale, picture.size.height)];
-//        }
-//        else if (picture.size.height > 1080)
-//        {
-//            picture = [picture rescaleImageToSize:CGSizeMake(1080*scale, 1080)];
-//        }
+        if (picture.size.height < 1080 )
+        {
+            picture = [picture rescaleImageToSize:CGSizeMake(picture.size.height*scale, picture.size.height)];
+        }
+        else if (picture.size.height > 1080)
+        {
+            picture = [picture rescaleImageToSize:CGSizeMake(1080*scale, 1080)];
+        }
         showImageView.frame = CGRectMake(showImageView.frame.origin.x, showImageView.frame.origin.y, 320*scale, 320);
     }
     else if (picture.size.width < picture.size.height)
     {
-//        if (picture.size.width < 1080)
-//        {
-//            picture = [picture rescaleImageToSize:CGSizeMake(picture.size.width, picture.size.width/scale)];
-//        }
-//        else if (picture.size.width > 1080)
-//        {
-//            picture = [picture rescaleImageToSize:CGSizeMake(1080, 1080/scale)];
-//        }
+        if (picture.size.width < 1080)
+        {
+            picture = [picture rescaleImageToSize:CGSizeMake(picture.size.width, picture.size.width/scale)];
+        }
+        else if (picture.size.width > 1080)
+        {
+            picture = [picture rescaleImageToSize:CGSizeMake(1080, 1080/scale)];
+        }
         
         showImageView.frame = CGRectMake(showImageView.frame.origin.x, showImageView.frame.origin.y, 320, 320/scale);
     }
